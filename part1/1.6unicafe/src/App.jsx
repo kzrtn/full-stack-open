6756e6fbedebdff2518258893ad7559ea79135e5
Jolene T <kartistenn2@gmail.com>
2026-06-30 04:58:30 +0000

    Fixed bugs and added positive calculation

--- part1/1.6unicafe/src/App.jsx
+++ part1/1.6unicafe/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>;
-
-const Total = ({good, neutral, bad}) = () => good+neutral+bad;
+const Total = (good, neutral, bad) => good + neutral + bad;
+const Average = (good, neutral, bad) => (good - bad) / (good + neutral + bad);
+const Positive = (good, neutral, bad) => (good / (good + neutral + bad)) * 100;
 
 const App = () => {
   // save clicks of each button to its own state
@@ -10,9 +11,9 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const UpGood = () => setGood(good += 1);
-  const UpBad = () => setBad(bad += 1);
-  const UpNeutral = () => setNeutral(neutral += 1);
+  const UpGood = () => setGood(good + 1);
+  const UpBad = () => setBad(bad + 1);
+  const UpNeutral = () => setNeutral(neutral + 1);
 
   return (
     <>
@@ -27,11 +28,14 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
-        <p>all {Total({good}, {neutral}, {bad})}</p>
+        <p>all {Total(good, neutral, bad)}</p>
+        <p>average {Average(good, neutral, bad)}</p>
+        <p>positive {Positive(good, neutral, bad)}</p>
       </div>
     </>
-
   );
 };
+
+
 
 export default App;
