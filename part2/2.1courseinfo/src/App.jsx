bc56d3b958f07f4a2460ec5311cdadca53d415db
Jolene T <kartistenn2@gmail.com>
2026-07-01 08:15:02 +0000

    Added 2.1courseinfo up to step 8 (total exercises count)

---
+++ part2/2.1courseinfo/src/App.jsx
@@ -0,0 +1,55 @@
+const Header = ({courseName}) => <h1>{courseName}</h1>
+
+const Content = ({parts}) => parts.map((part) => <Part part={part} key={part.id}/>)
+
+const Part = ({part}) => <p>{part.name} {part.exercises}</p>
+
+const Total = ({parts}) => {
+  const total = parts.reduce((sum, current) => sum + current.exercises, 0)
+  return (
+    <b>total of {total} exercises</b>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
