b06a0137edf93deea10d075e21d357597d2518a1
Jolene T <kartistenn2@gmail.com>
2026-07-02 09:16:41 +0000

    Finished 2.9. Added ability to filter people by name

--- part2/2.6phonebook/src/App.jsx
+++ part2/2.6phonebook/src/App.jsx
@@ -1,26 +1,58 @@
 import { useState } from 'react'
 
-const Contact = ({persons}) => persons.map(person => <div key={person.name}>{person.name}</div>)
+const Contact = ({persons}) => persons.map(person => {
+    if (person.visible) return <div key={person.name}>{person.name} {person.phone}</div>
+  }
+)
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'},
-    { name: 'John Smith'}
+    {
+      name: 'Arto Hellas',
+      phone: '123456789',
+      visible: true
+    },
+    {
+      name: 'John Smith',
+      phone:'987654321',
+      visible: true
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
   const inputboxName = (e) => setNewName(e.target.value)
+  const inputboxPhone = (e) => setNewPhone(e.target.value)
 
   const PersonAlreadyExists = (name) => {
-    const filtered = persons.filter((obj) => obj.name === name)
-    return filtered.length > 0 ? true : false
+    const filterPerson = persons.filter((obj) => obj.name === name)
+    return filterPerson.length === 0
+  }
+
+  const inputboxFilter = (e) => {
+
+    if (e.target.value !== '') {
+      // Goes through persons array object and checks if filtered name exists
+      // marks each element as visible or not based on filter
+      setPersons(persons.map(person => 
+        person.name.toUpperCase().includes(e.target.value.toUpperCase())
+        ? {...person, visible: true}
+        : {...person, visible: false}))
+    } else {
+      // If input box is empty, reset visibility, show all persons
+      setPersons(persons.map(person => ({...person,visible: true})))
+    }
   }
 
   const addName = (e) => {
     e.preventDefault()
 
-    if (!PersonAlreadyExists(newName)) {
-      const newPerson = { name: newName }
+    if (!PersonAlreadyExists) {
+      const newPerson = {
+        name: newName,
+        phone: newPhone,
+        visible: true
+      }
 
       setPersons([
         ...persons,
@@ -34,9 +66,15 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with 
+        <input onChange={inputboxFilter}/>
+      </div>
+      <h2>Add new number</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input onChange={inputboxName}/>
+          <div>name: <input onChange={inputboxName} /></div>
+          <div>number: <input onChange={inputboxPhone} /></div>
         </div>
         <div>
           <button type="submit">add</button>
