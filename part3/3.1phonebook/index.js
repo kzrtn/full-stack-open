bca072051e6c013ab8d66f1af672d752cffec073
Jolene T <kartistenn2@gmail.com>
2026-07-13 13:38:49 +0000

    finished 3.2. info page returns phonebook total and time request was received

---
+++ part3/3.1phonebook/index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  const phonebookTotal = persons.length
+  const date = new Date()
+  const dateString = date.toString()
+
+  response.send(`
+    <p>Phonebook has info for ${phonebookTotal} people</p>
+    <p>${dateString}</p>`)
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
