d6fb430f3bd217868548fd812f1fa093431739ae
Jolene T <kartistenn2@gmail.com>
2026-07-15 02:07:37 +0000

    added environment PORT to PORT var

--- part3/3.1phonebook/index.js
+++ part3/3.1phonebook/index.js
@@ -105,6 +105,6 @@ app.get('/info', (request, response) => {
     <p>${dateString}</p>`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
