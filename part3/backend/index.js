094794851b8e880ffa9e941af9df579f19539621
Jolene T <kartistenn2@gmail.com>
2026-07-18 02:58:01 +0000

    added ability to update and delete notes, added errorhandling

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -38,10 +38,20 @@ app.get('/api/notes', (request, response) => {
 })
 
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   const id = request.params.id
 
-  Note.findById(id).then(note => response.json(note))
+  Note.findById(id)
+    .then(note => {
+      if (note) {
+        note => response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      next(error)
+    })
   /*
   const note = notes.find(note => note.id === id)
 
@@ -53,12 +63,31 @@ app.get('/api/notes/:id', (request, response) => {
   */
 })
 
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
+
 app.delete('/api/notes/:id', (request, response) => {
   const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+  //notes = notes.filter(note => note.id !== id)
+  Note.findByIdAndDelete(id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
+
 
 const generateId = () => {
   const maxID = notes.length > 0
@@ -67,6 +96,27 @@ const generateId = () => {
 
   return String(maxID + 1)
 }
+
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const {content, important} = request.body
+  const id = request.params.id
+
+  Note.findById(id).then(note => {
+    if(!note) {
+      return response.status(404).end()
+    }
+
+    note.content = content
+    note.important = important
+
+    return note.save().then((updatedNote) => {
+      response.json(updatedNote)
+    })
+  })
+  .catch(error => next(error))
+})
+
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
