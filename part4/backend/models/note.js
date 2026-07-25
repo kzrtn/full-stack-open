94d3dfb2e78035f6bede54334b1c7c7262d7ab7c
Jolene T <kartistenn2@gmail.com>
2026-07-25 11:19:41 +0000

    forgot this one

--- part4/backend/models/note.js
+++ part4/backend/models/note.js
@@ -1,16 +1,6 @@
 const mongoose = require('mongoose')
-const config = require('../utils/config.js')
 
 mongoose.set('strictQuery', false)
-
-console.log('connecting to ', config.MONGODB_URI)
-mongoose.connect(config.MONGODB_URI, { family: 4 })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
   content: {
