01513aed9d9fb50a410d81803caac965eee382f1
Jolene T <kartistenn2@gmail.com>
2026-08-09 12:28:14 +0000

    changes

--- part4/backend/tests/note_api.test.js
+++ part4/backend/tests/note_api.test.js
@@ -1,22 +1,42 @@
-const { test, after } = require('node:test')
 const assert = require('node:assert')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app.js')
+const Note = require('../models/note.js')
 
 const api = supertest(app)
 
-test('notes are returned as json', async () => {
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false,
+  },
+  {
+    content: 'Browser can execute only Javascript',
+    important: true,
+  },
+]
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObj = new Note(initialNotes[0])
+  await noteObj.save()
+  noteObj = new Note(initialNotes[1])
+  await noteObj.save()
+})
+
+test.only('notes are returned as json', async () => {
   await api
     .get('/api/notes')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
 
-test('all notes are returned', async () => {
+test.only('all notes are returned', async () => {
   const response = await api.get('/api/notes')
 
-  assert.strictEqual(response.body.length, 2)
+  assert.strictEqual(response.body.length, initialNotes.length)
 })
 
 test('a specific note is within the returned notes', async () => {
