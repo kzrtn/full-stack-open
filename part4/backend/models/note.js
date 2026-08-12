7ce39ab7e1a68fe4c769c08f18cb23989009dcdd
Jolene T <kartistenn2@gmail.com>
2026-08-12 13:55:18 +0000

    creating new note adds it to user database too

--- part4/backend/models/note.js
+++ part4/backend/models/note.js
@@ -9,6 +9,10 @@ const noteSchema = new mongoose.Schema({
     required: true
   },
   important: Boolean,
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 noteSchema.set('toJSON', {
