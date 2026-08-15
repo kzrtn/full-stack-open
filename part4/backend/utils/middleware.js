7e6477c500be4b814ffff05bfaa2eb12b73bff37
Jolene T <kartistenn2@gmail.com>
2026-08-15 05:04:08 +0000

    added JsonWebTokenError handling

--- part4/backend/utils/middleware.js
+++ part4/backend/utils/middleware.js
@@ -20,6 +20,8 @@ const errorHandler = (error, req, res, next) => {
     return res.status(400).json({ error:error.message })
   } else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
     return res.status(400).json({ error: 'expected `username` to be unique' })
+  } else if (error.name === 'JsonWebTokenError') {
+    return res.status(401).json({ error: 'token invalid' })
   }
 
   next(error)
