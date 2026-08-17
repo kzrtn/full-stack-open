b580127a343276d91f69f2da7e11e42ce38bf6b0
Jolene T <kartistenn2@gmail.com>
2026-08-17 08:49:49 +0000

    fixed tests to work with new user data

--- part4/4.1bloglist/tests/blog_list_api.test.js
+++ part4/4.1bloglist/tests/blog_list_api.test.js
@@ -2,76 +2,28 @@ const assert = require('node:assert')
 const { describe, test, beforeEach, after } = require('node:test')
 
 const Blog = require('../models/blog.js')
+const User = require('../models/user.js')
 const app = require('../app.js')
+const { createSampleBlogs, blogsInDb } = require('./test_helper.js')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 
 const api = supertest(app)
 
-const sampleBlogs = [
-  {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: "5a422b891b54a676234d17fa",
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: "5a422ba71b54a676234d17fb",
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: "5a422bc61b54a676234d17fc",
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2,
-    __v: 0
-  }  
-]
-
 beforeEach(async () => {
-  await Blog.deleteMany({})
-  await Blog.insertMany(sampleBlogs)
+  await createSampleBlogs()
 })
 
 describe('viewing blogs', () => {
   test('returns correct amount of blogs', async () => {
+    const blogs = await blogsInDb()
+
     const res = await api
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
 
-    assert.strictEqual(res.body.length, sampleBlogs.length)
+    assert.strictEqual(res.body.length, blogs.length)
   })
 
   test('unique identifier property of blog posts is named id', async () => {
@@ -88,12 +40,16 @@ describe('viewing blogs', () => {
 
 describe('making blog posts', () => {
   test('able to create a new blog post', async () => {
-    const newBlog =   {
+    const dbAtStart = await blogsInDb()
+    const user = await User.findOne({ username: 'root' })
+
+    const newBlog = {
       id: "123456789",
       title: "This is a new blog post",
       author: "Test author",
       url: "https://www.google.com/",
       likes: 0,
+      user: user._id
     }
 
     await api
@@ -106,14 +62,17 @@ describe('making blog posts', () => {
       .expect(200)
       .expect('Content-Type', /application\/json/)
 
-    assert.strictEqual(res.body.length, sampleBlogs.length + 1)
+    assert.strictEqual(res.body.length, dbAtStart.length + 1)
   })
 
   test('if likes is missing from post request, it defaults to 0', async () => {
+    const user = await User.findOne({ username: 'root' })
+
     const newBlog = {
       title: "test post",
       author: "Test author",
       url: "https://www.google.com/",
+      user: user._id
     }
 
     await api
@@ -131,10 +90,13 @@ describe('making blog posts', () => {
   })
 
   test('response status 400 when blog is missing title', async () => {
+    const user = await User.findOne({ username: 'root' })
+
     const newBlog = {
       author: "wow",
       url: "https://www.kfc.com",
       likes: 1,
+      user: user._id
     }
 
     await api
@@ -144,10 +106,13 @@ describe('making blog posts', () => {
   })
 
   test('response status 400 when blog is missing url', async () => {
+    const user = await User.findOne({ username: 'root' })
+
     const newBlog = {
       title: "my title",
       author: "wow",
       likes: 10,
+      user: user._id
     }
 
     await api
@@ -159,6 +124,8 @@ describe('making blog posts', () => {
 
 describe('deleting blog posts', () => {
   test('response status 204 for successful single deletion', async () => {
+    const dbAtStart = await blogsInDb()
+
     const deleteBlog = {
       id: "5a422a851b54a676234d17f7",
     }
@@ -172,27 +139,31 @@ describe('deleting blog posts', () => {
       .expect(200)
       .expect('Content-Type', /application\/json/)
 
-    assert.strictEqual(res.body.length, sampleBlogs.length - 1)
+    assert.strictEqual(res.body.length, dbAtStart.length - 1)
   })
 
-  test('response status 400 if blog does not exist', async () => {
-    const deleteBlog = {
-      id: "123",
-    }
+  test('response status 404 if blog does not exist', async () => {
+    const dbAtStart = await blogsInDb()
 
+    const deleteBlog = {
+      id: "5a422a851b54a676234d1123",
+    }
+    
     await api
       .delete(`/api/blogs/${deleteBlog.id}`)
-      .expect(400)
+      .expect(404)
 
     const res = await api
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
 
-    assert.strictEqual(res.body.length, sampleBlogs.length)
+    assert.strictEqual(res.body.length, dbAtStart.length)
   })
 
-  test('response status 404 if id is not a valid ObjectId', async () => {
+  test('response status 400 if id is not a valid ObjectId', async () => {
+    const dbAtStart = await blogsInDb()
+
     const deleteBlog = {
       id: 123,
     }
@@ -206,18 +177,29 @@ describe('deleting blog posts', () => {
       .expect(200)
       .expect('Content-Type', /application\/json/)
 
-    assert.strictEqual(res.body.length, sampleBlogs.length)
+    assert.strictEqual(res.body.length, dbAtStart.length)
   })
 })
 
 describe('updating blog posts', () => {
   test('update single post', async () => {
+    const user = await User.findOne({ username: 'root' })
+
     const postToUpdate = {
       id: "5a422a851b54a676234d17f7",
       title: "React patterns",
       author: "Michael Chan",
       url: "https://reactpatterns.com/",
       likes: 8,
+    }
+
+    const expectedPost = {
+      ...postToUpdate,
+      user: {
+        id: user._id.toString(),
+        name: user.name,
+        username: user.username
+      }
     }
 
     await api
@@ -231,20 +213,28 @@ describe('updating blog posts', () => {
       .expect(200)
       .expect('Content-Type', /application\/json/)
 
-    assert.deepStrictEqual(postToUpdate, res.body[0])
+    const resultPost = res.body.find(blog => blog.id === '5a422a851b54a676234d17f7')
+    assert.deepStrictEqual(expectedPost, resultPost)
   })
 
-  test('response status 400 when blog does not exist', async () => {
-    const correctPost = {
+  test('response status 404 when blog does not exist', async () => {
+    const user = await User.findOne({ username: 'root' })
+
+    const expectedPost = {
       id: "5a422a851b54a676234d17f7",
       title: "React patterns",
       author: "Michael Chan",
       url: "https://reactpatterns.com/",
       likes: 7,
+      user: {
+        id: user._id.toString(),
+        name: user.name,
+        username: user.username
+      }
     }
 
     const postToUpdate = {
-      id: "123123123",
+      id: "5a422a851b54a676234d1234",
       title: "React patterns",
       author: "Michael Chan",
       url: "https://reactpatterns.com/",
@@ -255,23 +245,31 @@ describe('updating blog posts', () => {
       .put(`/api/blogs/`)
       .set('Content-Type', 'application/json')
       .send(postToUpdate)
-      .expect(400)
+      .expect(404)
 
     const res = await api
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
 
-    assert.deepStrictEqual(correctPost, res.body[0])
+    const resultPost = res.body.find(blog => blog.id === '5a422a851b54a676234d17f7')
+    assert.deepStrictEqual(expectedPost, resultPost)
   })
 
   test('response status 404 when id is not a valid ObjectId', async () => {
-    const correctPost = {
+    const user = await User.findOne({ username: 'root' })
+
+    const expectedPost = {
       id: "5a422a851b54a676234d17f7",
       title: "React patterns",
       author: "Michael Chan",
       url: "https://reactpatterns.com/",
       likes: 7,
+      user: {
+        id: user._id.toString(),
+        name: user.name,
+        username: user.username
+      }
     }
 
     await api
@@ -285,7 +283,8 @@ describe('updating blog posts', () => {
       .expect(200)
       .expect('Content-Type', /application\/json/)
 
-    assert.deepStrictEqual(correctPost, res.body[0])
+    const resultPost = res.body.find(blog => blog.id === '5a422a851b54a676234d17f7')
+    assert.deepStrictEqual(expectedPost, resultPost)
   })
 })
 
