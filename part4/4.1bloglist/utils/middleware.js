235004a40a30e1fc2824fdaf303142c5b4e9ce3e
Jolene T <kartistenn2@gmail.com>
2026-08-18 05:49:30 +0000

    added tokenExtractor middleware

--- part4/4.1bloglist/utils/middleware.js
+++ part4/4.1bloglist/utils/middleware.js
@@ -1,4 +1,5 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
 
 const requestLogger = (req, res, next) => {
   logger.info('Method: ', req.method)
@@ -25,8 +26,21 @@ const errorHandler = (err, req, res, next) => {
   next(err)
 }
 
+const tokenExtractor = (req, res, next) => {
+  const auth = req.get('authorization')
+
+  if (auth && auth.startsWith('Bearer ')) {
+    req.token = auth.replace('Bearer ', '')
+  } else {
+    req.token = null
+  }
+  
+  next()
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
