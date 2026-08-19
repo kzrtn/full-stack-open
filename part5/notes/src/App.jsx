fc14892b7608fcfa41bd521d472e4d2c91e655de
Jolene T <kartistenn2@gmail.com>
2026-08-19 01:43:02 +0000

    basic login form and login service

--- part5/notes/src/App.jsx
+++ part5/notes/src/App.jsx
@@ -3,12 +3,16 @@ import Note from './components/Note'
 import Notification from './components/Notification'
 import Footer from './components/Footer'
 import noteService from './services/notes'
+import loginService from './services/login'
 
 const App = () => { 
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     noteService
@@ -63,10 +67,43 @@ const App = () => {
     setNewNote(e.target.value)
   }
 
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    console.log('logging in with', username, password)
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+    } catch (error) {
+      setErrorMessage(`Invalid credentials: ${error}`)
+      //Clears error message after 5 seconds
+      setTimeout(() => setErrorMessage(null), 5000)
+    }
+    setUsername('')
+    setPassword('')
+  }
+
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+      
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>
+            username
+            <input type="text" value={username} onChange={({ target }) => setUsername(target.value)} />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input type="password" value={password} onChange={({ target }) => setPassword(target.value)} />
+          </label>
+        </div>
+        <button type="submit">login</button>
+      </form>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
