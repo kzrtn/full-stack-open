77e3326da5f596cabb070b2f4477507f6b1b77f6
Jolene T <kartistenn2@gmail.com>
2026-08-23 07:35:40 +0000

    added test for button click

--- part5/notes/src/components/Note.test.jsx
+++ part5/notes/src/components/Note.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import Note from './Note'
+import userEvent from '@testing-library/user-event'
 import { expect } from 'vitest'
 
 test('renders content', () => {
@@ -7,12 +8,21 @@ test('renders content', () => {
     content: 'Component testing is done with react-testing-library',
     important: true
   }
-
   const { container } = render(<Note note={note} />)
-
   const div = container.querySelector('.note')
-  
   screen.debug(div)
-
   expect(div).toHaveTextContent('Component testing is done with react-testing-library')
+})
+
+test('clicking the button calls event handler once', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+  const mockHandler = vi.fn()
+  render(<Note note={note} toggleImportance={mockHandler}/>)
+  const user = userEvent.setup()
+  const button = screen.getByText('make not important')
+  await user.click(button)
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
