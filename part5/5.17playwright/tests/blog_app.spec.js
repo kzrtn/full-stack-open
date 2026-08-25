92f94b97b5a5ed134e53f8eaebdc167857ff4c44
Jolene T <kartistenn2@gmail.com>
2026-08-25 07:20:17 +0000

    added tests for login

--- part5/5.17playwright/tests/blog_app.spec.js
+++ part5/5.17playwright/tests/blog_app.spec.js
@@ -1,17 +1,36 @@
 const { beforeEach, describe, expect, test } = require('@playwright/test')
+import { loginWith } from './helper'
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/test/reset')
+    await request.post('/api/users',
+      {
+        data: {
+          name: 'John Smith',
+          username: 'root',
+          password: 'Password@123'
+        }
+      }
+    )
     await page.goto('/')
   })
 
   test('login form is visible', async ({ page }) => {
-    const userInput = page.getByLabel('username')
-    const passwordInput = page.getByLabel('password')
-    const loginButton = page.getByRole('button', { name: 'Log in' })
+    await expect(page.getByLabel('username')).toBeVisible()
+    await expect(page.getByLabel('password')).toBeVisible()
+    await expect(page.getByRole('button', { name: 'Log in' })).toBeVisible()
+  })
 
-    expect(userInput).toBeVisible()
-    expect(passwordInput).toBeVisible()
-    expect(loginButton).toBeVisible()
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'root', 'Password@123')
+      await expect(page.getByText('John Smith is logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'root', 'Password')
+      await expect(page.getByText('Invalid credentials')).toBeVisible()
+    })
   })
 })
