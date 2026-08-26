be7f03ba09def94471779bf5a040cebbede89880
Jolene T <kartistenn2@gmail.com>
2026-08-26 02:36:03 +0000

    login returns user id in response

--- part4/4.1bloglist/controllers/tests.js
+++ part4/4.1bloglist/controllers/tests.js
@@ -7,4 +7,14 @@ testRouter.post('/reset', async (req, res) => {
   await Blog.deleteMany({})
   res.status(204).end()
 })
+
+testRouter.post('/reset-users', async (req, res) => {
+  await User.deleteMany({})
+  res.status(204).end()
+})
+
+testRouter.post('/reset-blogs', async (req, res) => {
+  await Blog.deleteMany({})
+  res.status(204).end()
+})
 module.exports = testRouter
