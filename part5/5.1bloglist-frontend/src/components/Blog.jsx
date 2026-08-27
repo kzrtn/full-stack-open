44cbb7e90cdfc90795672fc48d18c3b234af5a04
Jolene T <kartistenn2@gmail.com>
2026-08-27 08:08:36 +0000

    fixed reload bug and logged out user bug

--- part5/5.1bloglist-frontend/src/components/Blog.jsx
+++ part5/5.1bloglist-frontend/src/components/Blog.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
 const Blog = (props) => {
-  const [blog, setBlog] = useState(props.blog)
+  const [blog, setBlog] = useState(null)
   const navigate = useNavigate()
 
-  if (!blog || !props.user) {
-    return null
+  if (!blog && props.blog) {
+    setBlog(props.blog)
   }
+
+  if (!blog) return null
 
   const blogStyle = {
     paddingTop: 10,
@@ -34,6 +36,18 @@ const Blog = (props) => {
 
   const link = blog.url.includes('http') ? blog.url : `https://${blog.url}`
 
+  const removeButton = () => {
+    return (
+      <>
+        {
+          blog.user.id === props.user.id
+            ? <button onClick={deleteBlog}>remove</button>
+            : <></>
+        }
+      </>
+    )
+  }
+
   return (
     <div data-testid="blog" style={blogStyle}>
       <b>{blog.title}</b> By {blog.author}
@@ -42,8 +56,8 @@ const Blog = (props) => {
         likes {blog.likes}
         <button onClick={addLike}>like</button>
       </div>
-      <div>Added by {props.user.name}</div>
-      {blog.user.id === props.user.id ? <button onClick={deleteBlog}>remove</button> : <></>}
+      <div>Added by {blog.user.name}</div>
+      {props.user && removeButton()}
     </div>
   )
 }
