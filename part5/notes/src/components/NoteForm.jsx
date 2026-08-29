b6130e10433baf4a73e1d165d298c58c202434a3
Jolene T <kartistenn2@gmail.com>
2026-08-29 01:11:41 +0000

    updated styling

--- part5/notes/src/components/NoteForm.jsx
+++ part5/notes/src/components/NoteForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { TextField, Button } from '@mui/material'
 
 const NoteForm = ({createNote}) => {
   const [newNote, setNewNote] = useState('')
@@ -21,14 +22,24 @@ const NoteForm = ({createNote}) => {
   }
 
   return (
-  <form onSubmit={addNote}>
-    <input
-      value={newNote}
-      onChange={handleNoteChange}
-      placeholder='write note content here'
-    />
-    <button>save</button>
-  </form>
+  <div>
+    <h2>
+      Create a new note
+    </h2>
+    <form onSubmit={addNote}>
+      <TextField
+        label="note content"
+        value={newNote}
+        onChange={handleNoteChange}
+      />
+      <div>
+        <Button type="submit" variant="contained" style={{marginTop: 10}}>
+          save
+        </Button>
+      </div>
+    </form>
+  </div>
+  
 )}
 
 export default NoteForm
