bcc7018b48507ab7d5786645a3ee9963f37ea575
Jolene T <kartistenn2@gmail.com>
2026-08-31 04:55:29 +0000

    finished example before exercises

--- part6/zustand-example/src/components/Controls.jsx
+++ part6/zustand-example/src/components/Controls.jsx
@@ -1,15 +1,13 @@
-import { useCounterStore } from './store'
+import { useCounterControls } from './store'
 
 const Controls = () => {
-  const increment = useCounterStore(state => state.increment)
-  const decrement = useCounterStore(state => state.decrement)
-  const zero = useCounterStore(state => state.zero)
+  const { increment, decrement, reset } = useCounterControls()
 
   return (
     <div>
       <button onClick={increment}>plus</button>
       <button onClick={decrement}>minus</button>
-      <button onClick={zero}>zero</button>
+      <button onClick={reset}>zero</button>
     </div>
   )
 }
