8af525802c7e79ed63e32eb5d8b5a945527fa548
Jolene T <kartistenn2@gmail.com>
2026-08-31 04:44:25 +0000

    separated components

--- part6/zustand-example/src/App.jsx
+++ part6/zustand-example/src/App.jsx
@@ -1,26 +1,11 @@
-import { create } from 'zustand'
-
-const useCounterStore = create(set => ({
-  counter: 0,
-  increment: () => set(state => ({ counter: state.counter + 1 })),
-  decrement: () => set(state => ({ counter: state.counter - 1 })),
-  reset: () => set({ counter: 0 })
-}))
+import Display from './components/Display'
+import Controls from './components/Controls'
 
 const App = () => {
-  const counter = useCounterStore(state => state.counter)
-  const increment = useCounterStore(state => state.increment)
-  const decrement = useCounterStore(state => state.decrement)
-  const reset = useCounterStore(state => state.reset)
-
   return (
     <>
-      <div>{counter}</div>
-      <div>
-        <button onClick={increment}>plus</button>
-        <button onClick={decrement}>minus</button>
-        <button onClick={reset}>zero</button>
-      </div>
+      <Display />
+      <Controls />
     </>
   )
 }
