fd7b5c41da5b2d0c5a91076d87348d84f6b68e7b
Jolene T <kartistenn2@gmail.com>
2026-09-08 01:56:14 +0000

    initial commit

---
+++ part6/redux-counter/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <div>0</div>
+      <div>
+        <button>plus</button>
+        <button>minus</button>
+        <button>zero</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
