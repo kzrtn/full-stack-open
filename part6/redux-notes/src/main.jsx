9cc5810bc10e6bfd0977b581feba25213e88855d
Jolene T <kartistenn2@gmail.com>
2026-09-08 04:35:25 +0000

    moved noteReducer into its own file

--- part6/redux-notes/src/main.jsx
+++ part6/redux-notes/src/main.jsx
@@ -1,15 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { createStore } from 'redux'
-
-const noteReducer = (state = [], action) => {
-  switch (action.type) {
-    case 'NEW_NOTE':
-      state.push(action.payload)
-      return state
-    default:
-      return state
-  }
-}
+import noteReducer from './reducers/noteReducer'
 
 const store = createStore(noteReducer)
 
