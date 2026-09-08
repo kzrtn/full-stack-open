825104ec41a49373399a944d26ee5c472da8863e
Jolene T <kartistenn2@gmail.com>
2026-09-08 04:49:50 +0000

    added test and toggle importance function to reducer

--- part6/redux-notes/src/reducers/noteReducer.js
+++ part6/redux-notes/src/reducers/noteReducer.js
@@ -1,7 +1,15 @@
 const noteReducer = (state = [], action) => {
   switch (action.type) {
     case 'NEW_NOTE':
-      return state.concat(action.payload)
+      return [...state, action.payload]
+    case 'TOGGLE_IMPORTANCE':
+      const id = action.payload.id
+      const noteToChange = state.find(n => n.id === id)
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important
+      }
+      return state.map(note => note.id === id ? changedNote : note)
     default:
       return state
   }
