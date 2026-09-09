0c9e9aa48f07797454dfa7d0bdf4dd35a76ec6a9
Jolene T <kartistenn2@gmail.com>
2026-09-09 06:43:46 +0000

    added filter notes by important etc functionality

--- part6/redux-notes/src/App.jsx
+++ part6/redux-notes/src/App.jsx
@@ -1,33 +1,12 @@
 import Notes from './components/Notes'
 import NoteForm from './components/NoteForm'
+import VisibilityFilter from './components/VisibilityFilter'
 
 const App = () => {
-  const filterSelected = (value) => {
-    console.log(value)
-  }
   return (
     <div>
       <NoteForm />
-      <div>
-        <input
-          type="radio"
-          name="filter"
-          onChange={() => filterSelected('ALL')}
-        />
-        all
-        <input
-          type="radio"
-          name="filter"
-          onChange={() => filterSelected('IMPORTANT')}
-        />
-        important
-        <input
-          type="radio"
-          name="filter"
-          onChange={() => filterSelected('NONIMPORTANT')}
-        />
-        nonimportant
-      </div>
+      <VisibilityFilter />
       <Notes />
     </div>
   )
