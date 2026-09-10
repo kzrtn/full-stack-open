efcad53e58c05ced7c4c785c42fd7875650497cd
Jolene T <kartistenn2@gmail.com>
2026-09-10 05:44:50 +0000

    refactored main.jsx to use configureStore from redux toolkit

--- part6/redux-notes/src/main.jsx
+++ part6/redux-notes/src/main.jsx
@@ -1,19 +1,10 @@
 import { createRoot } from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './App'
-import noteReducer from './reducers/noteReducer'
-import filterReducer from './reducers/filterReducer'
+import store from './store'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
-})
-
-const store = createStore(reducer)
-
-const root = createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
   </Provider>
