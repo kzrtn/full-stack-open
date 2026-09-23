b9a6a4557e534bda8724cc7ed7b0c605297dbd9a
Jolene T <kartistenn2@gmail.com>
2026-09-23 03:15:54 +0000

    added phone filter

--- part8/phonebook/index.js
+++ part8/phonebook/index.js
@@ -46,9 +46,14 @@ const typeDefs = /* GraphQL */`
     id: ID!
   }
 
+  enum YesNo {
+    YES
+    NO
+  }
+
   type Query {
     personCount: Int!
-    allPersons: [Person!]!
+    allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
   }
 `
@@ -56,7 +61,13 @@ const typeDefs = /* GraphQL */`
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+      if(!args.phone) {
+        return persons
+      }
+      const byPhone = (person) => args.phone === 'YES' ? person.phone : !person.phone
+      return persons.filter(byPhone)
+    },
     findPerson: (root, args) => persons.find(p => p.name === args.name)
   },
   Person: {
